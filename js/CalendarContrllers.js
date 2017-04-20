077737c45c7184a452511f60268b43a3a584db52
KrunalSoni3006 <kunalsoni00@gmail.com>
2017-04-20 13:44:39 +0530

    next prev year

---
+++ js/CalendarContrllers.js
@@ -0,0 +1,31 @@
+  var Months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+
+  angular.module("Calendar", []).controller("CalendarCTRL", function() {
+    var CalendarAccess = this;
+    var x = Months[new Date().getMonth()] + " " + new Date().getFullYear();
+    CalendarAccess.CurrentHeader = x;
+    CalendarAccess.PrevBtn = function() {
+      CheckNextPrevAction(CalendarAccess.CurrentHeader);
+    };
+    CalendarAccess.NextBtn = function() {
+      CheckNextPrevAction(CalendarAccess.CurrentHeader);
+    };
+
+    CheckNextPrevAction = function(Value) {
+      var pattern = new RegExp("^\d");
+      if (!pattern.test(Value)) {
+        var year = Value.split(" ")[1];
+        var Month = Value.split(" ")[0];
+        if ($.inArray(Month, Months) != -1 && $.inArray(Month, Months) == 11) {
+          CalendarAccess.CurrentHeader = Months[0] + " " + (parseInt(year) + 1);
+        } else {
+          CalendarAccess.CurrentHeader = Months[$.inArray(Month, Months) + 1] + " " + year;
+        }
+      }
+    }
+
+  });
--- js/Calender.js
+++
@@ -1,4 +0,0 @@
-angular.module("Calender",[]).controller("CalenderCTRL",function () {
-  var CalenderAcess = this;
-  CalenderAcess.CurrentHeader = new Date();
-});
